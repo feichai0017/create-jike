8eb8fe0753552d27119d537b3b5ae24344eedea7
Eric <Ericsgc@outlook.com>
2024-08-03 17:05:51 +1000

    token set

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,20 @@
-import {Button, Card, Form, Input} from "antd";
+import {Button, Card, Form, Input, message} from "antd";
 import logo from '../../assets/logo.png'
 import './index.scss'
+import {fetchLogin} from "../../store/modules/user";
+import {useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
-    const onFinish = (values) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const onFinish = async (values) => {
         console.log(values)
+        await dispatch(fetchLogin(values))
+
+        navigate('/')
+        message.success('login successfully')
     }
 
 
@@ -14,35 +24,24 @@ const Login = () => {
                 <img className="login-logo" src={logo} alt="logo" />
                 <Form validateTrigger={['onBlur']} onFinish={onFinish}>
                     <Form.Item
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'please enter your username',
-                        },
-                    ]}>
-                        <Input size='large' placeholder="Username"/>
-                    </Form.Item>
-                    <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'please enter your password',
-                        },
-                    ]}>
-                        <Input size='large' placeholder="Password"/>
-                    </Form.Item>
-                    <Form.Item
-                    name='mobile'
-                    rules={[
-                        { required: true, message: '请输入手机号' },
-                        {
-                            pattern: /^1[3-9]\d{9}$/,
-                            message: 'Incorrect type of phone number',
-                        }
-                    ]}>
+                        name='mobile'
+                        rules={[
+                            { required: true, message: '请输入手机号' },
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: 'Incorrect type of phone number',
+                            }
+                        ]}
+                    >
                         <Input size="large" placeholder="Please enter your phone number" />
+                    </Form.Item>
+                    <Form.Item
+                        name="code"
+                        rules={[
+                            { required: true, message: 'please enter your code' },
+                        ]}
+                    >
+                        <Input size="large" placeholder="please enter password" maxLength={6} />
                     </Form.Item>
                     <br/>
                     <Form.Item>
